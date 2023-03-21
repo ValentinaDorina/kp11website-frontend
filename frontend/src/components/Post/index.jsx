ae8a74affa2d0dc36e13800ca3f13a429ecd712b
ValentinaDorina <dorina_04@mail.ru>
2023-03-21 17:34:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ frontend/src/components/Post/index.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
+import clsx from 'clsx';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Clear';
+import DownloadIcon from '@mui/icons-material/Download';
+import EditIcon from '@mui/icons-material/Edit';
+import EyeIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import CommentIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+
+import styles from './Post.module.scss';
+import { UserInfo } from '../UserInfo';
+import { PostSkeleton } from './Skeleton';
+import { fetchRemovePost } from '../../redux/slices/posts';
+
+export const Post = ({
+  id,
+  title,
+  createdAt,
+  imageUrl,
+  fileUrl,
+  user,
+  viewsCount,
+  commentsCount,
+  tags,
+  children,
+  isFullPost,
+  isLoading,
+  isEditable,
+}) => {
+  const dispatch = useDispatch();
+
+  if (isLoading) {
+    return <PostSkeleton />;
+  }
+
+  const onClickRemove = () => {
+    if (window.confirm('Вы действительно хотите удалить статью?')) {
+      dispatch(fetchRemovePost(id));
+    }
+  };
+  console.log(fileUrl);
+  return (
+    <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
+      {isEditable && (
+        <div className={styles.editButtons}>
+          <Link to={`/posts/${id}/edit`}>
+            <IconButton color='primary'>
+              <EditIcon />
+            </IconButton>
+          </Link>
+          <IconButton onClick={onClickRemove} color='secondary'>
+            <DeleteIcon />
+          </IconButton>
+        </div>
+      )}
+      {imageUrl && (
+        <img
+          className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
+          src={imageUrl}
+          alt={title}
+        />
+      )}
+
+      <div className={styles.wrapper}>
+        <div className={styles.PostHeader}>
+          <UserInfo {...user} additionalText={createdAt} />
+
+          <form action={fileUrl}>
+            <Button
+              className={styles.DownloadButton}
+              variant='outlined'
+              type='submit'
+              size='large'
+              endIcon={<DownloadIcon />}>
+              {' '}
+              Скачать{' '}
+            </Button>
+          </form>
+        </div>
+
+        <div className={styles.indention}>
+          <h2 className={clsx(styles.title, { [styles.titleFull]: isFullPost })}>
+            {isFullPost ? title : <Link to={`/posts/${id}`}>{title}</Link>}
+          </h2>
+          <ul className={styles.tags}>
+            {tags.map((name) => (
+              <li key={name}>
+                <Link to={`/tag/${name}`}>#{name}</Link>
+              </li>
+            ))}
+          </ul>
+
+          {children && <div className={styles.content}>{children}</div>}
+          <ul className={styles.postDetails}>
+            <li>
+              <EyeIcon />
+              <span>{viewsCount}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
